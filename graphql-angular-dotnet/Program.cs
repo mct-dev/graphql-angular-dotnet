bad4d768f4936aaaf702c96468e0c701882c6e84
Mike Tobias <axis139@gmail.com>
2019-08-06 09:38:26 -0700

    Add project files.

---
+++ graphql-angular-dotnet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using GraphQL;
+using GraphQL.Types;
+
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    var schema = Schema.For(@"
+      type Query {
+        hello: String
+      }
+    ");
+
+    var json = schema.Execute(_ =>
+    {
+      _.Query = "{ hello }";
+      _.Root = new { Hello = "Hello World!" };
+    });
+
+    Console.WriteLine(json);
+    Console.Read();
+  }
+}
